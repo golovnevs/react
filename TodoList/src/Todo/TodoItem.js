997279467f4675c2c9c39789d80b4ae185bf1f3b
GolovnevSergei <golovnevs@gmail.com>
2021-12-02 13:04:23 +0100

    upload files

---
+++ TodoList/src/Todo/TodoItem.js
@@ -0,0 +1,54 @@
+import React, {useContex} from 'react'
+import Context from './context'
+import { useContext } from 'react'
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '.5rem 1rem',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '.5rem'
+  },
+  input: {
+    marginRight: '1rem'
+  },
+  button: {
+    colour: 'red'
+  }
+
+}
+
+function TodoItem({ todo, index, onChange }) {
+  const { removeTodo } = useContext(Context)
+  const classes = []
+
+  if(todo.completed) {
+    classes.push('done')
+  }
+
+
+  console.log(todo)
+  return (
+    <li style={styles.li}>
+      <span className={classes.join(' ')}>
+      <input 
+      type = 'checkbox' 
+      checked = {todo.completed}
+      style={styles.input} 
+      onChange={() => onChange(todo.id)}/>
+        <strong>{ index + 1 + '.  '}</strong>
+        {todo.title}
+      </span>
+      <button className='rm' onClick={() => removeTodo(todo.id)}>x</button>
+    </li>
+  )
+}
+
+// TodoItem.PropTypes = {
+//   todo: PropTypes.object.isRequired,
+//   index: PropTypes.number
+// }
+
+export default TodoItem
